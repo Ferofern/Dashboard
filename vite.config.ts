ecb481f42d4e8313a70dd28be2f9df35072fc590
Felix Romero <ferofern@espol.edu.ec>
2025-08-12 05:50:49 +0000

    dddd

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/Dashboard/',  // coincide con el repo y base href
+  base: '/Dashboard/',   // <-- Aquí la D en mayúscula
   plugins: [
     react(),
     VitePWA({
       registerType: 'autoUpdate',
       devOptions: { enabled: true },
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'favicon-32x32.png', 'favicon-16x16.png'],
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
       manifest: {
         id: '/Dashboard/',
         start_url: '/Dashboard/',
