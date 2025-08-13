56098a639e03ec2874806f13979c3a5cf1980ed4
Felix Romero <ferofern@espol.edu.ec>
2025-08-13 14:16:29 +0000

    f

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e755d862'], (function (workbox) { 'use strict';
     "revision": "21199adf659a9b979572e74b6e36502f"
   }, {
     "url": "/dashboard/index.html",
-    "revision": "0.lgtsti96teo"
+    "revision": "0.6hvd1necar8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/dashboard/index.html"), {
